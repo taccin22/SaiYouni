16324713e38837e3e3ef8f53b77be14d35a660ac
Duc Vinh <vinhduc0304@gmail.com>
2024-10-15 23:47:21 +0700

    combine Week-03

---
+++ Assets/_Data/_Scripts/EnemyCtrl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyCtrl : MyBehaviour
+{
+    [SerializeField] protected NavMeshAgent agent;
+    public NavMeshAgent Agent => agent;
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadAgent();
+    }
+
+    protected virtual void LoadAgent()
+    {
+        if (this.agent != null) return;
+        this.agent = GetComponent<NavMeshAgent>();
+        Debug.Log(transform.name + ": LoadAgent", gameObject);
+    }
+}
